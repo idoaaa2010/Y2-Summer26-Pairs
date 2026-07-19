6fabffc49cf8f675a74866d4fe3e5ac077c9bb19
IdoAv27-meet <idoaaa2010@gmail.com>
2026-07-19 20:58:52 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ChatWindow from "./components/ChatWindow";
 import ChatInput from "./components/ChatInput";
 import TelemetrySidebar from "./components/TelemetrySidebar";
 //import { streamChat } from "./lib/api";
-import { playRadioBleep, playGearShift, setMuted } from "./lib/audio";
+//import { playRadioBleep, playGearShift, setMuted } from "./lib/audio";
 import { getTrack } from "./data/tracks";
 import type { ChatMessage, ChatMode, Telemetry } from "./types";
 
